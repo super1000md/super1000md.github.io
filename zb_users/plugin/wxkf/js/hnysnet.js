e8ab7a80ed0eb954121904365b19e16c3382241a
super1000md <super1000md@users.noreply.github.com>
2023-02-23 16:39:37 +0800

    2

---
+++ zb_users/plugin/wxkf/js/hnysnet.js
@@ -0,0 +1,1 @@
+function copywx(){const range=document.createRange();range.selectNode(document.getElementById('copy_content'));const selection=window.getSelection();if(selection.rangeCount>0)selection.removeAllRanges();selection.addRange(range);document.execCommand('copy');alert("复制成功！")}
